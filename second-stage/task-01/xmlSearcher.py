226d636a8c91793eb4dd88c90cdaeaa4a9315a7c
admhome <admhome@ya.ru>
2018-08-06 14:53:18 +0300

    First task from 2nd stage

---
+++ second-stage/task-01/xmlSearcher.py
@@ -0,0 +1,68 @@
+# -*- coding: UTF-8 -*-
+
+# К элементам иногда добавляются Атрибуты. Атрибуты состоят из пары имя-значение, где значение берется в двойные кавычки ("), вот так: type="dessert". Атрибуты позволяют сохранять вместе с элементом дополнительные параметры, меняя значения этих параметров от элемента к элементу в одном и том же документе.
+
+import sys
+import argparse
+import xml.etree.ElementTree as ET
+import os
+
+neededSumm = 0;
+neededCount = 0;
+
+def createParser():
+    parser = argparse.ArgumentParser(prog='XML Searcher')
+    parser.add_argument('-f', '--file', '--xml-file', type=argparse.FileType('r'))
+ 
+    return parser
+
+
+# def parseXML(data):
+#     summ = 0
+#     count = 0
+
+#     for dataItem in xmlRoot:
+#         if dataItem.attrib:
+#             if dataItem.attrib['value2']:
+#                 summ += int(dataItem.attrib['value2'])
+#                 count += 1
+
+#         if dataItem.childNodes:
+#             for child in dataItem.childNodes:
+#                 avgRes = parseXML(child)
+
+#                 summ += avgRes["summ"]
+#                 count += avgRes["count"]
+
+#     return {
+#         "summ": summ,
+#         "count": count
+#     }
+
+
+def findInXML(data):
+    summ = 0
+    count = 0
+    searchRes = data.findall(".//*[@value2]")
+
+    for searchItem in searchRes:
+        summ += int(searchItem.attrib['value2'])
+        count += 1
+
+    return {
+        "summ": summ,
+        "count": count
+    }
+
+
+if __name__ == '__main__':
+    parser = createParser()
+    xmlFile = parser.parse_args(sys.argv[1:])
+
+    fileContent = xmlFile.file.read()
+
+    xmlRoot = ET.fromstring(fileContent)
+
+    avgData = findInXML(xmlRoot)
+
+    print('avegRes: ' + str(avgData["summ"] / avgData["count"]))
