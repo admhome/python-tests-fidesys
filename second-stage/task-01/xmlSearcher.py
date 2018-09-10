34bb73ca73019470957893953f3fb793a9a1ee84
admhome <admhome@ya.ru>
2018-09-10 09:07:27 +0300

    change file choose mechanic

--- second-stage/task-01/xmlSearcher.py
+++ second-stage/task-01/xmlSearcher.py
@@ -1,6 +1,8 @@
 # -*- coding: UTF-8 -*-
 
-# К элементам иногда добавляются Атрибуты. Атрибуты состоят из пары имя-значение, где значение берется в двойные кавычки ("), вот так: type="dessert". Атрибуты позволяют сохранять вместе с элементом дополнительные параметры, меняя значения этих параметров от элемента к элементу в одном и том же документе.
+# К элементам иногда добавляются Атрибуты. Атрибуты состоят из пары имя-значение, где значение берется в двойные кавычки ("),
+# вот так: type="dessert". Атрибуты позволяют сохранять вместе с элементом дополнительные параметры, меняя значения этих
+# параметров от элемента к элементу в одном и том же документе.
 
 import sys
 import argparse
@@ -10,11 +12,11 @@ import os
 neededSumm = 0;
 neededCount = 0;
 
-def createParser():
-    parser = argparse.ArgumentParser(prog='XML Searcher')
-    parser.add_argument('-f', '--file', '--xml-file', type=argparse.FileType('r'))
- 
-    return parser
+# def createParser():
+#     parser = argparse.ArgumentParser(prog='XML Searcher')
+#     parser.add_argument('-f', '--file', '--xml-file', type=argparse.FileType('r'))
+#
+#     return parser
 
 
 def findInXML(data):
@@ -33,12 +35,13 @@ def findInXML(data):
 
 
 if __name__ == '__main__':
-    parser = createParser()
-    xmlFile = parser.parse_args(sys.argv[1:])
+    # parser = createParser()
+    # xmlFile = parser.parse_args(sys.argv[1:])
+    # fileContent = xmlFile.file.read()
+    # xmlRoot = ET.fromstring(fileContent)
 
-    fileContent = xmlFile.file.read()
-
-    xmlRoot = ET.fromstring(fileContent)
+    xmlFile = sys.argv[1]
+    xmlRoot = ET.parse(xmlFile).getroot()
 
     avgData = findInXML(xmlRoot)
 
